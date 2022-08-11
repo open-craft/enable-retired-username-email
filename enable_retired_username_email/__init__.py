421275e22f5ab37ecb2693265ecfa3e363043050
Agrendalath <piotr@surowiec.it>
2022-08-11 21:13:27 +0200

    fix: remove unnecessary version pins
    They were breaking Maple deployments.

--- enable_retired_username_email/__init__.py
+++ enable_retired_username_email/__init__.py
@@ -4,6 +4,6 @@ Custom Django app to enable reuse of username and email after retirement.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 default_app_config = 'enable_retired_username_email.apps.EnableRetiredUsernameEmailConfig'
